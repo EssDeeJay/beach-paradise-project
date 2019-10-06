1a61ba1006227100d95b8709fe206042164b73d5
Shreyank Jadiya <shreyankjadiya120@gmail.com>
2019-10-06 01:03:08 -0400

    Initial Commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,241 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+ // Query for nodes to use in creating pages.
+ resolve(
+   graphql(request).then(result => {
+     if (result.errors) {
+       reject(result.errors)
+     }
+     return result;
+   })
+ )
+});
+
+// Implement the Gatsby API "createPages". This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ actions, graphql }) => {
+ const { createPage } = actions;
+
+// Create pages for each blog.
+ const getBlog = makeRequest(graphql, `
+   {
+     allContentfulBlog (
+       sort: { fields: [createdAt], order: DESC }
+       filter: {
+         node_locale: {eq: "en-US"}},)
+     {
+       edges {
+         node {
+           id
+           slug
+         }
+       }
+     }
+   }
+   `).then(result => {
+   result.data.allContentfulBlog.edges.forEach(({ node }) => {
+     createPage({
+       path: `blog/${node.slug}`,
+       component: path.resolve(`src/templates/blog.js`),
+       context: {
+         id: node.id,
+       },
+     })
+   })
+});
+
+// Archive page for all blogs, including pagination
+const getArchive = makeRequest(graphql, `
+{
+  allContentfulBlog (
+    sort: { fields: [createdAt], order: DESC }
+    filter: {
+      node_locale: {eq: "en-US"}},)
+  {
+    edges {
+      node {
+        id
+        slug
+      }
+    }
+  }
+}
+`).then(result => {
+  const blogs = result.data.allContentfulBlog.edges
+  const blogsPerPage = 9
+  const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: path.resolve("./src/templates/archive.js"),
+      context: {
+        limit: blogsPerPage,
+        skip: i * blogsPerPage,
+        numPages,
+        currentPage: i + 1
+      },
+    })
+  })
+});
+
+// Create guide category page, including pagination
+const getGuide = makeRequest(graphql, `
+{
+  allContentfulBlog (
+    sort: { fields: [createdAt], order: DESC }
+    filter: {
+      node_locale: {eq: "en-US"}
+      category: {elemMatch: {title: {eq: "Guide"}}}
+    },)
+  {
+    edges {
+      node {
+        id
+        slug
+      }
+    }
+  }
+}
+`).then(result => {
+  const blogs = result.data.allContentfulBlog.edges
+  const blogsPerPage = 9
+  const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/category/guide` : `/category/guide/${i + 1}`,
+      component: path.resolve("./src/templates/guide.js"),
+      context: {
+        limit: blogsPerPage,
+        skip: i * blogsPerPage,
+        numPages,
+        currentPage: i + 1
+      },
+    })
+  })
+});
+
+// Create tickets category page, including pagination
+const getTickets = makeRequest(graphql, `
+{
+  allContentfulBlog (
+    sort: { fields: [createdAt], order: DESC }
+    filter: {
+      node_locale: {eq: "en-US"}
+      category: {elemMatch: {title: {eq: "Tickets"}}}
+    },)
+  {
+    edges {
+      node {
+        id
+        slug
+      }
+    }
+  }
+}
+`).then(result => {
+  const blogs = result.data.allContentfulBlog.edges
+  const blogsPerPage = 9
+  const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/category/tickets` : `/category/tickets/${i + 1}`,
+      component: path.resolve("./src/templates/tickets.js"),
+      context: {
+        limit: blogsPerPage,
+        skip: i * blogsPerPage,
+        numPages,
+        currentPage: i + 1
+      },
+    })
+  })
+});
+
+// Create directions category page, including pagination
+const getDirections = makeRequest(graphql, `
+{
+  allContentfulBlog (
+    sort: { fields: [createdAt], order: DESC }
+    filter: {
+      node_locale: {eq: "en-US"}
+      category: {elemMatch: {title: {eq: "Directions"}}}
+    },)
+  {
+    edges {
+      node {
+        id
+        slug
+      }
+    }
+  }
+}
+`).then(result => {
+  const blogs = result.data.allContentfulBlog.edges
+  const blogsPerPage = 9
+  const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/category/directions` : `/category/directions/${i + 1}`,
+      component: path.resolve("./src/templates/directions.js"),
+      context: {
+        limit: blogsPerPage,
+        skip: i * blogsPerPage,
+        numPages,
+        currentPage: i + 1
+      },
+    })
+  })
+});
+
+// Create food category page, including pagination
+const getFood = makeRequest(graphql, `
+{
+  allContentfulBlog (
+    sort: { fields: [createdAt], order: DESC }
+    filter: {
+      node_locale: {eq: "en-US"}
+      category: {elemMatch: {title: {eq: "Food"}}}
+    },)
+  {
+    edges {
+      node {
+        id
+        slug
+      }
+    }
+  }
+}
+`).then(result => {
+  const blogs = result.data.allContentfulBlog.edges
+  const blogsPerPage = 9
+  const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/category/food` : `/category/food/${i + 1}`,
+      component: path.resolve("./src/templates/food.js"),
+      context: {
+        limit: blogsPerPage,
+        skip: i * blogsPerPage,
+        numPages,
+        currentPage: i + 1
+      },
+    })
+  })
+});
+
+
+ return Promise.all([
+   getBlog,
+   getArchive,
+   getGuide,
+   getTickets,
+   getDirections,
+   getFood
+  ])
+};
+
